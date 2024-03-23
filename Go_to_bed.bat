31cd4d1e6f262f0b1cff0ee973cc452b3f10b013
God_Electric <131879932+GodElectric@users.noreply.github.com>
2024-03-23 15:13:45 +0100

    Delete Go_to_bed.bat

--- Go_to_bed.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-rem I think of it as a virus that a normal person simply cannot remove
-set end=0
-start "" "start_.vbs"
-set /p end=Go to bed, and with PC (n/y)
-if %end% == n exit
-start "" "WinNTuser.bat"
-rem I'm not serious :D
